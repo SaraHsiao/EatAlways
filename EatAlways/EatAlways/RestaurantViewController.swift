0ce9e45fbfade5687ee4a6412c9082e69f5021d2
Sara <kafeidou0909@gmail.com>
2017-08-16 01:07:01 +0800

    UI for Restaurant List

---
+++ EatAlways/EatAlways/RestaurantViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RestaurantViewController.swift
+//  EatAlways
+//
+//  Created by KaFeiDou on 2017/8/16.
+//  Copyright © 2017年 KaFeiDou. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
