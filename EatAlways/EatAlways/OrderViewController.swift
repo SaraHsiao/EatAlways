57a9a45d6b61ef90a462d40755d4049ea33a35c2
Sara <kafeidou0909@gmail.com>
2017-08-16 21:29:44 +0800

    Design Order Layout

---
+++ EatAlways/EatAlways/OrderViewController.swift
@@ -0,0 +1,34 @@
+//
+//  OrderViewController.swift
+//  EatAlways
+//
+//  Created by KaFeiDou on 2017/8/16.
+//  Copyright © 2017年 KaFeiDou. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class OrderViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+extension OrderViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OrderItemCell", for: indexPath)
+        
+        return cell
+    }
+}
