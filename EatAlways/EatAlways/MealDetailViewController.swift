c4bb6934aecd62f4386d89d2b37143b3e415aee6
Sara <kafeidou0909@gmail.com>
2017-08-16 14:54:22 +0800

    Design Meal Details Layout

---
+++ EatAlways/EatAlways/MealDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MealDetailViewController.swift
+//  EatAlways
+//
+//  Created by KaFeiDou on 2017/8/16.
+//  Copyright © 2017年 KaFeiDou. All rights reserved.
+//
+
+import UIKit
+
+class MealDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
