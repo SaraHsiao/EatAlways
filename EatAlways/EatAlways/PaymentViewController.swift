24a03cfeb2fa34b369a6b3bf4ac0b268fa77556f
Sara <kafeidou0909@gmail.com>
2017-08-16 20:41:27 +0800

    Design Credit Card Layout

---
+++ EatAlways/EatAlways/PaymentViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PaymentViewController.swift
+//  EatAlways
+//
+//  Created by KaFeiDou on 2017/8/16.
+//  Copyright © 2017年 KaFeiDou. All rights reserved.
+//
+
+import UIKit
+import Stripe
+
+class PaymentViewController: UIViewController {
+    
+    @IBOutlet weak var cardTextField: STPPaymentCardTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
