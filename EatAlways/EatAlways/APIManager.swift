3883700de550990f3bb72506a5866eed11f830ca
Sara <kafeidou0909@gmail.com>
2017-08-21 21:58:11 +0800

    Create API to get List of Restaurants

--- EatAlways/EatAlways/APIManager.swift
+++ EatAlways/EatAlways/APIManager.swift
@@ -42,7 +42,7 @@ class APIManager {
                 
                 let jsonData = JSON(value)
                 self.accessToken = jsonData["access_token"].string!
-                self.refreshToken = jsonData["refreshToken"].string!
+                self.refreshToken = jsonData["refresh_token"].string!
                 self.expired = Date().addingTimeInterval(TimeInterval(jsonData["expires_in"].int!))
                 
                 completionHandler(nil)
@@ -58,13 +58,13 @@ class APIManager {
     // API to log and user out
     func logout(completionHandler: @escaping (NSError?) -> Void) {
         
-        let path = "api/social/voken-token/"
+        let path = "api/social/revoke-token/"
         let url = baseURL!.appendingPathComponent(path)
         
         let params:[String:Any] = [
             "client_id": CLIENT_ID,
             "client_secret": CLIENT_SECRET,
-            "token": self.accessToken       //this token is return server, not facebook
+            "token": self.accessToken   //this token is return server, not facebook
         ]
         Alamofire.request(url!, method: .post, parameters: params, encoding: JSONEncoding.default, headers: nil).responseString { (response) in
             
@@ -72,11 +72,65 @@ class APIManager {
             case .success:
                 completionHandler(nil)
                 break
-            
+                
             case .failure(let error):
                 completionHandler(error as NSError?)
                 break
             }
+        }
+    }
+    
+    // API to refresh the token when it's expired
+    func refreshTokenIfNeed (completionHandler: @escaping () -> Void) {
+        
+        let path = "api/social/refresh-token/"
+        let url = baseURL?.appendingPathComponent(path)
+        
+        let params:[String:Any] = [
+            "access_token":self.accessToken,
+            "refresh_token": self.refreshToken,
+            ]
+        if (Date() > self.expired) {
+            
+            Alamofire.request(url!, method: .post, parameters: params, encoding: JSONEncoding.default, headers: nil).responseJSON(completionHandler: { (response) in
+                
+                switch response.result {
+                case .success(let value):
+                    let jsonData = JSON(value)
+                    self .accessToken = jsonData["access_token"].string!
+                    self.expired = Date().addingTimeInterval(TimeInterval(jsonData["expires_in"].int!))
+                    completionHandler()
+                    break
+                case .failure:
+                    break
+                }
+            })
+        } else {
+            completionHandler()
+        }
+    }
+    
+    // API Getting Restaurants list
+    func getRestaurants (completionHandler: @escaping (JSON) -> Void) {
+        
+        let path = "api/customer/restaurants/"
+        let url = baseURL?.appendingPathComponent(path)
+        
+        refreshTokenIfNeed {
+            
+            Alamofire.request(url!, method: .get , parameters: nil, encoding: JSONEncoding.default, headers: nil).responseJSON(completionHandler: { (response) in
+                
+                switch response.result {
+                case .success(let value):
+                    let jsonData = JSON(value)
+                    completionHandler(jsonData)
+                    break
+                    
+                case .failure:
+                    completionHandler(nil)
+                    break
+                }
+            })
         }
     }
 }
