c1e663b0a52a2ba71dc22f52a20b9edbb9ad5d35
Sara <kafeidou0909@gmail.com>
2017-08-23 23:15:39 +0800

    Design SideMenu and OrdersList Layout

---
+++ EatAlways/EatAlways/DriverOrderViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DriverOrderViewController.swift
+//  EatAlways
+//
+//  Created by KaFeiDou on 2017/8/23.
+//  Copyright © 2017年 KaFeiDou. All rights reserved.
+//
+
+import UIKit
+
+class DriverOrderViewController: UITableViewController {
+    
+    @IBOutlet weak var menuBarButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if revealViewController() != nil {
+            menuBarButton.target = self.revealViewController()
+            menuBarButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DriverOrderCell", for: indexPath)
+        
+        return cell
+    }
+}
+
