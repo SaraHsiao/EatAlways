fdbfcbb22ca85fa3d8c3f562fe31318020671396
Sara <kafeidou0909@gmail.com>
2017-08-24 19:19:22 +0800

    Showing Customer Details on Drivers Map

--- EatAlways/EatAlways/DeliveryViewController.swift
+++ EatAlways/EatAlways/DeliveryViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 import MapKit
 
 class DeliveryViewController: UIViewController {
+    
+    var orderId: Int?
+    
     @IBOutlet weak var menuBarButton: UIBarButtonItem!
     
     @IBOutlet weak var lblCustomerName: UILabel!
@@ -32,6 +35,47 @@ class DeliveryViewController: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
+        loadData()
     }
-
+    
+    func loadData() {
+        
+        APIManager.shared.getCurrentDriverOrder { (json) in
+            print(json)
+            
+            let order = json["order"]
+            if let id = order["id"].int, order["status"] == "On the way" {
+                self.orderId = id
+                
+                let from = order["address"].string
+                let to = order["restaurant"]["address"].string
+                
+                let customerName = order["customer"]["name"].string
+                let customerAvatar = order["customer"]["avatar"].string
+                let customerAddress = order["address"].string
+                
+                self.lblCustomerName.text = customerName
+                self.lblCustomerAddress.text = from
+                self.imgCustomerAvatar.image = try! UIImage(data: Data(contentsOf: URL(string: customerAvatar!)!))
+                self.imgCustomerAvatar.layer.cornerRadius = 50 / 2
+                self.imgCustomerAvatar.clipsToBounds = true
+                
+            } else {
+                
+                self.map.isHidden = true
+                self.viewInfo.isHidden = true
+                self.btnComplete.isHidden = true
+                
+                // Showing a message here
+                let lblMessage = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 60))
+                lblMessage.center = self.view.center
+                lblMessage.textAlignment = .center
+                lblMessage.textColor = UIColor.blue
+                lblMessage.font = UIFont.init(name: "Marker Felt", size:20)
+                lblMessage.text = "Your don't have any orders for delivery."
+                
+                self.view.addSubview(lblMessage)
+            }
+        }
+    }
 }
