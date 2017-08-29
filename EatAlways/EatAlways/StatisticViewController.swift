7f0136fbd632c8381f6b6b7bcec7646cbe9bb122
Sara <kafeidou0909@gmail.com>
2017-08-29 15:30:12 +0800

    Showing Chart of Driver's revenue

--- EatAlways/EatAlways/StatisticViewController.swift
+++ EatAlways/EatAlways/StatisticViewController.swift
@@ -14,6 +14,8 @@ class StatisticViewController: UIViewController {
     @IBOutlet weak var menuBarButton: UIBarButtonItem!
     @IBOutlet weak var viewChart: BarChartView!
     
+    var weekdays = ["Sun","Mon","Tue","Wed","Thurs","Fri","Sat"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,5 +25,54 @@ class StatisticViewController: UIViewController {
             
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
+        // 1.Initialize Chart
+        self.initializeChart()
+        
+        // 2. Load Data to Charts
+        self.loadDataToChart()
     }
+    
+    func initializeChart() {
+        
+        viewChart.noDataText = "No Data"
+        viewChart.animate(xAxisDuration: 2.0, yAxisDuration: 2.0, easingOption: .easeInBounce)
+        viewChart.xAxis.labelPosition = .bottom
+        viewChart.descriptionText = ""
+        
+        viewChart.legend.enabled = false
+        viewChart.scaleXEnabled = false
+        viewChart.scaleYEnabled = false
+        viewChart.pinchZoomEnabled = false
+        viewChart.doubleTapToZoomEnabled = false
+        
+        viewChart.leftAxis.axisMinValue = 0.0
+        viewChart.leftAxis.axisMaxValue = 100.0
+        viewChart.highlighter = nil
+        viewChart.rightAxis.enabled = false
+        viewChart.xAxis.drawGridLinesEnabled = false
+    }
+    
+    func loadDataToChart() {
+        
+        APIManager.shared.getDriverRevenue { (json) in
+            if (json != nil) {
+                let revenue = json["revenue"]
+                
+                var dataEntries: [BarChartDataEntry] = []
+                
+                for i in 0..<self.weekdays.count {
+                    let day = self.weekdays[i]
+                    let dataEntry = BarChartDataEntry(x: revenue[day].double!, yValues: [Double(i)])
+                    dataEntries.append(dataEntry)
+                }
+                let chartDataSet = BarChartDataSet(values: dataEntries, label: "Revenue a day")
+                chartDataSet.colors = ChartColorTemplates.material()
+                
+                let chartData = BarChartData(dataSet: chartDataSet)
+                
+                self.viewChart.data = chartData
+            }
+        }
+    }
+    
 }
